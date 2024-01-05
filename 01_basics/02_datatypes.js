051e70c8fc50e6a4c703142ae31d65d9cf58dd53
NishantChambyal <146934202+NishantChambyal@users.noreply.github.com>
2024-01-05 20:37:46 +0000

    data types

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; // use all JS code as a newer version
+// alert(3+3) // it is not work here because we use node here not browser
+
+console.log(3+3)
+console.log("nishant")
+
+// premitive data types
+
+let a = "nishant"  //string type
+let b = 5454   //number type
+let c = true  // boolean type
+let d = BigInt // big integer type
+let e = null  // object type (stand alone value)
+let f = undefined // undefine type
+let g = Symbol // define uniqueness type
+
+
+// object
+
+console.log(typeof "nishant")
