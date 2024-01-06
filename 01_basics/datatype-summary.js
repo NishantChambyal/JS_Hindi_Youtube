aad78630c62b5d0afd3cc416fb4a76f312332984
NishantChambyal <146934202+NishantChambyal@users.noreply.github.com>
2024-01-06 12:53:15 +0000

    data type summary

---
+++ 01_basics/datatype-summary.js
@@ -0,0 +1,38 @@
+// # Primitive 
+
+// 7 types :  string , number, boolean, null , undefined , symbol , BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+
+let userEmail;
+
+const id = Symbol("123")
+const anotherId = Symbol("123")
+
+console.log(id === anotherId);
+
+const bigNumber = 98746264954654n
+
+
+
+// Reference (Non Primitive)
+
+// Array, Object, Functions
+
+const heros = ["shaktiman", "nagraj", "doga"]
+
+let myObj = {
+    name: "nishant",
+    age: 22
+}
+
+const myFunction = function (){
+    console.log("hello world")
+
+}
+
+console.log(typeof bigNumber);  // symbol
