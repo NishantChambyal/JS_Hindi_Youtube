61f22b03b9bd60457612a189aba31d665fa5fe4e
NishantChambyal <146934202+NishantChambyal@users.noreply.github.com>
2024-03-21 05:24:15 +0000

    little changes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,66 +1,37 @@
-let score = 33
+// let score = 33
+// let score = "33abc" //Not a number NaN
+// let score = null  // value of null is zero after convert into number
+// let score = undefine // return NaN after convert into Number
+// let score = true // return 1  if true
+// let score = false // return 0  if false
+// let score = "string" // NaN
+let score = "33"
 
+console.log(typeof(score));  // both are same
 console.log(typeof score);
-console.log(typeof (score));  // same check method
 
-let valueInNumber = Number (score)
+let valueInNumber = Number(score)  // score var is now converted into number
 
 console.log(typeof valueInNumber);
+console.log(valueInNumber);
 
-
-// "33" => 33 
-// "33abc" => NaN (not a Number but type "33abc" is number)
-// true => 1 & false => 0 
+// "33" => 33
+// "33abc" => NaN
+// "33abc" => NaN        but type is Number
+// true => 1 ;  false => 0
 
 let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
 
-// 1 => true; 0 => false
-// " " => false
+console.log(typeof booleanIsLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1 => true ;  0 => false
+// "" => false
 // "Nishant" => true
 
 let someNumber = 33 
-let stringNumber = String (someNumber) // 33 is number but now it is converted into "33" type of 33 is now string
-
-console.log(stringNumber);
-
-// ************************* Operations ***********************
-
-let value = 3
-let negValue = -value
-
-// console.log(negValue);
-
-// console.log(2+2);
-// console.log(2-2);
-// console.log(2*2);
-// console.log(2**2);
-// console.log(2/2);
-// console.log(2%2);
-
-let str1 = "hello"
-let str2 = "nishant"
-
-let str3 = str1 + str2
-
-console.log(str3);
-
-console.log("1" + 2);  // 12 output
-console.log(1 + "2"); // 12 output
-console.log("1" + 2 + 2); // 122 output  (because it is treated as string)
-console.log(1 + 2 + "2"); // 32 output (because it is treated as number)
-
-console.log((3 + 4) * 5 % 3);
-
-console.log(+true); // 1 output
-console.log(+""); // 0 output
-
-let num1 , num2 , num3 
-
-num1 = num2= num3 = 2 + 2 // smart way to define variables but not recomended to code like this because of readability
-
-let gameCounter = 100
-gameCounter++;  // post increment
-console.log(gameCounter);
+let StringNumber = String(someNumber)
+console.log(typeof StringNumber);
+console.log(StringNumber);
