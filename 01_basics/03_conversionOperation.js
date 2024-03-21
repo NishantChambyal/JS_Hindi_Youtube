2e6f6108190661977145c273a8a25e0ef5c47d03
NishantChambyal <146934202+NishantChambyal@users.noreply.github.com>
2024-03-21 05:52:50 +0000

    changes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -35,3 +35,51 @@ let someNumber = 33
 let StringNumber = String(someNumber)
 console.log(typeof StringNumber);
 console.log(StringNumber);
+
+
+// ************************** OPERATIONS ********************************
+
+let value = 3 
+let negValue =  -value
+
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3); // two square of 3
+// console.log(2/2); // divide
+// console.log(2%2); // find remainder  (modulas)
+
+let str1 = "hello"
+let str2 = " nishant"
+
+let str3 = str1 + str2
+
+console.log(str3);
+
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);  //  output is 122  starting number is string
+console.log(1 + 2 + "2");  //  output is 32   starting number is number and 2nd is also a number last one is string because of rules of java script
+
+
+console.log( (3 + 4) * 5 % 3);
+
+
+console.log(+true); // return 1 
+console.log(true+); // return error
+console.log(+"");  // 0
+  // do not write code like this
+
+  let num1 , num2 , num3
+
+  num1 = num2 = num3 = 2+2  // dont write code like this
+
+
+  let gameCounter = 100
+
+  gameCounter++;  // post-fix increment
+//   ++gameCounter;  // pre-fix increment
+  console.log(gameCounter);
