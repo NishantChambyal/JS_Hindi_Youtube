589bf2044f6dd701c0377166fa2fcab8d7a087aa
NishantChambyal <146934202+NishantChambyal@users.noreply.github.com>
2024-03-22 06:41:12 +0000

    changes in strings

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -1,37 +1,38 @@
-const name = "hitesh"
-const repoCount = 50
+const name = "nishant"
+const repocount =  50
 
-console.log(name + repoCount + " value")
+// console.log(name + repocount + "Value"); // THIS IS OLD METHOD TO CONCATINATE THE STRING
 
-console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+console.log(`Hello my name is ${name} and this is my repocunt ${repocount}`); // THIS IS THE MODERN WAY TO WRITE
 
-const gameName = new String('nishant~hc~com')
+const gameName = new String("nishant-hc-com")
 
-console.log(gameName[0])
-console.log(gameName.__proto__)
+console.log(gameName[0]);
+console.log(gameName.__proto__);
 
 
+// strings Methods
 
 console.log(gameName.length);
-console.log(gameName.toLocaleUpperCase);
-console.log(gameName.charAt(2))
-// consol.log(gameName.indexOf('t'))
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(6));
+console.log(gameName.indexOf("t"));
 
-const newString = gameName.substring(0, 4);
+const newString = gameName.substring(0,4)
 console.log(newString);
 
-const anotherString = gameName.slice(0, -5)
-console.log(newString);
+const anotherString = gameName.slice(-8,4)
+console.log(anotherString);
 
-
-const newStringOne = "   nishant   "
+const newStringOne = "   nishant    "
 console.log(newStringOne);
-console.log(newString.trim());
+console.log(newStringOne.trim());
 
 const url = "https://nishant.com/nishant%20chambyal"
-console.log(url.replace( "%20", "-" ));
 
-console.log(url.includes("nishant"))
+console.log(url.replace('%20', '$'));
 
-console.log(gameName.split("~"))
+console.log(url.includes('nishant'));
+
+console.log(gameName.split('-'));
 
